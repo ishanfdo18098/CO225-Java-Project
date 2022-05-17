ca6dc89f1b625da1251ccc280b2c546f0a0ffb8f
ridmajayasundara <e18155@eng.pdn.ac.lk>
2022-05-17 22:57:53 +0530

    Bid value Selecting Activity Done:

---
+++ BidCoinApp/app/src/main/java/com/ishanadeeparidma/bidcoin/Bidding/BiddingActivity.java
@@ -0,0 +1,14 @@
+package com.ishanadeeparidma.bidcoin.Bidding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BiddingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bidding);
+    }
+}
