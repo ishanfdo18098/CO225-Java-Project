c94bfab1745b18088b465ae007610a303ab7b2ce
NipunFernando <e18100@eng.pdn.ac.lk>
2022-05-18 14:51:22 +0530

    AuctionWinPage

---
+++ BidCoinApp/app/src/main/java/com/ishanadeeparidma/bidcoin/AuctionWin/AuctionWinActivity.java
@@ -0,0 +1,14 @@
+package com.ishanadeeparidma.bidcoin.AuctionWin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.ishanadeeparidma.bidcoin.R;
+import android.os.Bundle;
+
+public class AuctionWinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auction_win);
+    }
+}
