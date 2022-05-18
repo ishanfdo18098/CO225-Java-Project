6a9efb8be0cc98773bba17f443c51b4c336b6b26
ridmajayasundara <e18155@eng.pdn.ac.lk>
2022-05-18 15:35:50 +0530

    Admin Page 4 - stated time added

--- BidCoinApp/app/src/main/java/com/ishanadeeparidma/bidcoin/AdminMode/AdminPage4.java
+++ BidCoinApp/app/src/main/java/com/ishanadeeparidma/bidcoin/AdminMode/AdminPage4.java
@@ -17,8 +17,8 @@ import java.util.Calendar;
 
 public class AdminPage4 extends AppCompatActivity {
 
-    EditText date_in;
-    EditText time_in;
+
+
     EditText date_time_in;
 
     @Override
@@ -28,8 +28,7 @@ public class AdminPage4 extends AppCompatActivity {
         date_time_in=findViewById(R.id.date_time_input);
 
         date_time_in.setInputType(InputType.TYPE_NULL);
-        date_in.setInputType(InputType.TYPE_NULL);
-        time_in.setInputType(InputType.TYPE_NULL);
+
 
         date_time_in.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,37 +67,8 @@ public class AdminPage4 extends AppCompatActivity {
 
     }
 
-    private void showTimeDialog(final EditText time_in) {
-        final Calendar calendar=Calendar.getInstance();
 
-        TimePickerDialog.OnTimeSetListener timeSetListener=new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                calendar.set(Calendar.HOUR_OF_DAY,hourOfDay);
-                calendar.set(Calendar.MINUTE,minute);
-                SimpleDateFormat simpleDateFormat=new SimpleDateFormat("HH:mm");
-                time_in.setText(simpleDateFormat.format(calendar.getTime()));
-            }
-        };
 
-        new TimePickerDialog(AdminPage4.this,timeSetListener,calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),false).show();
-    }
 
-    private void showDateDialog(final EditText date_in) {
-        final Calendar calendar=Calendar.getInstance();
-        DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                calendar.set(Calendar.YEAR,year);
-                calendar.set(Calendar.MONTH,month);
-                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
-                SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yy-MM-dd");
-                date_in.setText(simpleDateFormat.format(calendar.getTime()));
-
-            }
-        };
-
-        new DatePickerDialog(AdminPage4.this,dateSetListener,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
-    }
 
 }
